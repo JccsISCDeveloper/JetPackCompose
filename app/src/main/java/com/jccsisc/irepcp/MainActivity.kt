e8971970b2a4d9aa007c25ffc36256768e3c6b2c
jccsISC <silva.jccs18@gmail.com>
2022-11-14 09:56:02 -0600

    Creacion del proytecto

---
+++ app/src/main/java/com/jccsisc/irepcp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.jccsisc.irepcp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jccsisc.irepcp.ui.theme.IREPCPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IREPCPTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    IREPCPTheme {
+
+    }
+}
