e4a82bfa6fef2d709de9ccdf3a464a5e2591d6fd
jccsISC <silva.jccs18@gmail.com>
2022-11-23 10:54:51 -0600

    Practica en Reports LazyColumn y LazyRow

--- app/src/main/java/com/jccsisc/irepcp/ui/features/reports/ReportsScreen.kt
+++ app/src/main/java/com/jccsisc/irepcp/ui/features/reports/ReportsScreen.kt
@@ -1,19 +1,28 @@
 package com.jccsisc.irepcp.ui.features.reports
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jccsisc.irepcp.ui.theme.BlackAbi
+import com.jccsisc.irepcp.ui.theme.GrayBg
 
 /**
  * Project: IREPCP
@@ -22,30 +31,106 @@ import androidx.compose.ui.unit.dp
  */
 @Composable
 fun ReportsScreen() {
-    var num by rememberSaveable { mutableStateOf(0) }
+    LazyColumn(
+        contentPadding = PaddingValues(vertical = 10.dp, horizontal = 15.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
 
-    Column {
-        Text(text = "Conteo: $num")
+        item {
+            Text(
+                text = "Productos de Super Mercado",
+                style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold, color = BlackAbi)
+            )
+        }
 
-        Divider(thickness = 20.dp, color = Color.Transparent)
-
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-            Counter(buttonTitle = "Aumentar conteo") {
-                num++
-            }
-            Counter("Disminuir conteo") {
-                num--
+//        items(listaProductos) { producto ->
+//            CardProducto(producto = producto)
+//        }
+        itemsIndexed(listaProductos) { posicion, producto ->
+            if (posicion % 5 == 0 && posicion != 0) {
+                LazyRow {
+                    items(listaPublicidad) { publicidad ->
+                        CardPublicidad(publicidad = publicidad)
+                    }
+                }
+            } else {
+                CardProducto(producto = producto)
             }
         }
     }
 }
 
+data class Producto(val nombre: String, val precio: Double)
+data class Publicidad(val titulo: String)
+
+private val listaProductos = listOf(
+    Producto(nombre = "Manzana", precio = 18.99),
+    Producto(nombre = "Avena 4kg", precio = 150.43),
+    Producto(nombre = "Longaniza Kg", precio = 80.00),
+    Producto(nombre = "Pera Kg", precio = 46.90),
+    Producto(nombre = "Piña Kg", precio = 70.00),
+    Producto(nombre = "Pepino Kg", precio = 30.50),
+    Producto(nombre = "Mayonesa", precio = 56.00),
+    Producto(nombre = "Atún 8PCK", precio = 160.43),
+    Producto(nombre = "Cereal", precio = 97.63),
+    Producto(nombre = "Leche LALA 12PCK", precio = 315.00),
+    Producto(nombre = "Azúcar", precio = 53.00),
+    Producto(nombre = "Salmón", precio = 359.96),
+    Producto(nombre = "Agua 112PCK", precio = 135.50)
+)
+
+private val listaPublicidad = listOf(
+    Publicidad("Publicidad 1"),
+    Publicidad("Publicidad 2"),
+    Publicidad("Publicidad 3"),
+    Publicidad("Publicidad 4")
+)
+
 @Composable
-fun Counter(buttonTitle: String, updateCount: () -> Unit) {
-    Button(onClick = {
-        updateCount()
-    }) {
-        Text(text = buttonTitle)
+fun CardProducto(producto: Producto) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp),
+        shape = RoundedCornerShape(14.dp),
+        elevation = 3.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(GrayBg)
+                .padding(6.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = producto.nombre,
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Medium)
+            )
+            Text(
+                text = "${producto.precio} MX",
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Light,
+                    fontStyle = FontStyle.Italic
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun CardPublicidad(publicidad: Publicidad) {
+    Box(
+        modifier = Modifier
+            .padding(end = 8.dp)
+            .size(140.dp)
+            .clip(RoundedCornerShape(12))
+            .background(
+                Color.LightGray
+            ), contentAlignment = Alignment.Center
+    ) {
+        Text(text = publicidad.titulo, style = TextStyle(fontWeight = FontWeight.Black))
     }
 }
 
