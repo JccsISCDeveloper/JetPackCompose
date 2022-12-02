4cd51e7a04dfd09970e7ddbb251d9349eb957918
jccsISC <silva.jccs18@gmail.com>
2022-12-02 12:14:08 -0600

    Integrando tab a DetailsFavoritesscreen

--- app/src/main/java/com/jccsisc/irepcp/ui/screens/dashboard/detailsfavorites/DetailsFavoritesScreen.kt
+++ app/src/main/java/com/jccsisc/irepcp/ui/screens/dashboard/detailsfavorites/DetailsFavoritesScreen.kt
@@ -1,27 +1,75 @@
 package com.jccsisc.irepcp.ui.screens.dashboard.detailsfavorites
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import com.jccsisc.irepcp.ui.theme.GrayBg
+import androidx.compose.ui.res.painterResource
+import com.google.accompanist.pager.*
+import com.jccsisc.irepcp.ui.screens.dashboard.detailsfavorites.TabItem.*
+import kotlinx.coroutines.launch
 
 /**
  * Project: IREPCP
  * FROM: com.jccsisc.irepcp.ui.screens.dashboard.detailsfavorites
  * Created by Julio Cesar Camacho Silva on 30/11/22
  */
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun DetailsFavoritesScreen(producto: String) {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(GrayBg), contentAlignment = Alignment.Center
+    val tabs = listOf(
+        ItemDescription,
+        ItemBenefits,
+        ItemRecipes
+    )
+
+    val pagerState = rememberPagerState()
+
+    Column() {
+        Tabs(tabs, pagerState)
+        TabsContent(tabs, pagerState)
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun Tabs(tabs: List<TabItem>, pagerState: PagerState) {
+    val scope = rememberCoroutineScope()
+
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+            )
+        }
     ) {
-        Text(text = producto, fontSize = 26.sp, fontWeight = FontWeight.ExtraBold)
+        tabs.forEachIndexed { index, tabItem ->
+            LeadingIconTab(
+                selected = pagerState.currentPage == index,
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                },
+                icon = {
+                    Image(
+                        painter = painterResource(id = tabItem.icon),
+                        contentDescription = ""
+                    )
+                },
+                text = { Text(text = tabItem.title, style = MaterialTheme.typography.overline) })
+        }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabsContent(tabs: List<TabItem>, pagerState: PagerState) {
+    HorizontalPager(state = pagerState, count = tabs.size) { page ->
+        tabs[page].screen()
     }
 }
