2ffc070d9e20ad5ad4212336111ef1b455886c8b
jccsISC <silva.jccs18@gmail.com>
2022-12-12 11:07:56 -0600

    Agregando xtension function para formato de la fecha y hora, ademas mostrar hace cuanto tiempo se modificO dicha tarea.

--- app/src/main/java/com/jccsisc/irepcp/utils/ExtensionFunctions.kt
+++ app/src/main/java/com/jccsisc/irepcp/utils/ExtensionFunctions.kt
@@ -1,5 +1,6 @@
 package com.jccsisc.irepcp.utils
 
+import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
@@ -10,6 +11,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.jccsisc.irepcp.IREPApp
+import com.jccsisc.irepcp.R
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Project: IREPCP
@@ -26,6 +31,7 @@ import com.jccsisc.irepcp.IREPApp
 fun SpacerApp(size: Int) {
     Spacer(modifier = Modifier.size(size.dp))
 }
+
 /**
  * Manipular el color y texto de la barra de tareas
  * */
@@ -48,3 +54,67 @@ fun showToast(msg: String, duration: Int = Toast.LENGTH_SHORT) {
 /**
  * -------------------- Extesion functions
  * */
+
+/**
+ * Devuelve la fecha de System.currentTimeMillis() = 9/12/2022 13:44
+ * */
+fun Long.timeMillisToFormatDate(): String = DateFormat.getInstance().format(Date(this))
+
+/**
+ * Devuelve el tiempo que ha transcurrido
+ * desde la fecha de creacion.
+ * Result:
+ * 1.- Hoy, 12:04
+ * 2.- Ayer a las 14:22
+ * 3.- Miercoles
+ * 4.- 07 Diciembre
+ * */
+fun Long.lastModifiedTime(): String {
+    val ctx: Context = IREPApp.INSTANCE
+    val nowDate =  System.currentTimeMillis()
+    val lastDate = this
+
+    val seconds = ( nowDate - lastDate ) / 1000
+    val minutes = seconds / 60
+    val hours = minutes / 60
+    val days = hours / 24
+
+    fun time(date: Long): String {
+        val sdf = SimpleDateFormat("HH:mm a", Locale.forLanguageTag("es-MX"))
+        return sdf.format(date)
+    }
+
+    fun day(date: Long): String {
+        val sdf = SimpleDateFormat("EEEE", Locale.forLanguageTag("es-MX"))
+        return sdf.format(date)
+    }
+
+    fun isToday(lastDate: Long, date: Long): String {
+        val sdf = SimpleDateFormat("u", Locale.forLanguageTag("ex-MX"))
+        val today = sdf.format(lastDate)
+        val yesterday = sdf.format(nowDate)
+
+        return if (today == yesterday) {
+            ctx.getString(R.string.str_today, time(lastDate))
+        } else {
+            ctx.getString(R.string.str_yesterday, time(lastDate))
+        }
+    }
+
+    fun dayMonth(date: Long): String {
+        val sdf = SimpleDateFormat("dd MMMM", Locale.forLanguageTag("es-MX"))
+        return sdf.format(date)
+    }
+
+    return if (days > 1) {
+        if (days == 2L) {
+            day(lastDate)
+        } else {
+            dayMonth(lastDate)
+        }
+    } else if (days == 1L) {
+        ctx.getString(R.string.str_yesterday_at, time(lastDate))
+    } else {
+        isToday(lastDate, nowDate)
+    }
+}
