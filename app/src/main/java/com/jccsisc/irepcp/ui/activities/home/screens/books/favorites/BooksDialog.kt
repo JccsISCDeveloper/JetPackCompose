a1486b89d3f7b6810f0feb35ceb8acbcfdc95091
jccsISC <silva.jccs18@gmail.com>
2022-12-21 18:03:46 -0600

    Redisenio para agregar un book, se implementO la funcion para obtener imagen desde la galeria del dispositivo

--- app/src/main/java/com/jccsisc/irepcp/ui/activities/home/screens/books/favorites/BooksDialog.kt
+++ app/src/main/java/com/jccsisc/irepcp/ui/activities/home/screens/books/favorites/BooksDialog.kt
@@ -1,24 +1,35 @@
 package com.jccsisc.irepcp.ui.activities.home.screens.books.favorites
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.jccsisc.irepcp.IREPApp
 import com.jccsisc.irepcp.R
 import com.jccsisc.irepcp.core.constants.Constants
 import com.jccsisc.irepcp.ui.activities.home.screens.books.home.domain.model.Book
-import com.jccsisc.irepcp.ui.theme.GrayBg
-import com.jccsisc.irepcp.ui.theme.PrimaryColor
-import com.jccsisc.irepcp.utils.setCoilImagePainter
+import com.jccsisc.irepcp.ui.theme.*
 
 /**
  * Project: IREPCP
@@ -31,8 +42,16 @@ fun BooksDialog(
     onCheckBoxSelected: (selected: Boolean) -> Unit,
     addBook: (book: Book) -> Unit
 ) {
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    var bitmap = remember { mutableStateOf<Bitmap?>(null) }
+
     var imageBook by remember { mutableStateOf(Constants.NO_VALUE) }
     var favoriteBook by remember { mutableStateOf(false) }
+
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+            imageUri = uri
+        }
 
     Dialog(
         onDismissRequest = { },
@@ -50,30 +69,75 @@ fun BooksDialog(
             ) {
                 Text(text = "Agregar libro", style = MaterialTheme.typography.h5)
                 Spacer(modifier = Modifier.height(14.dp))
+
+                imageUri?.let {
+                    if (Build.VERSION.SDK_INT < 28) {
+                        bitmap.value =
+                            MediaStore.Images.Media.getBitmap(IREPApp.INSTANCE.contentResolver, it)
+                    } else {
+                        val source = ImageDecoder.createSource(IREPApp.INSTANCE.contentResolver, it)
+                        bitmap.value = ImageDecoder.decodeBitmap(source)
+                    }
+                }
+
                 Box(
                     modifier = Modifier
-                        .background(Color.Gray)
-                        .size(200.dp)
+                        .clip(RoundedCornerShape(6.dp))
+                        .background(Gray50)
+                        .width(180.dp)
+                        .height(200.dp)
                 ) {
-                    Image(
-                        painter = setCoilImagePainter(image = imageBook),
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop
+                    bitmap.value?.asImageBitmap()?.let {
+                        Image(
+                            bitmap = it,
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                    IconToggleButton(
+                        checked = favoriteBook,
+                        onCheckedChange = {
+                            favoriteBook = it
+                            onCheckBoxSelected(favoriteBook)
+                        },
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .size(40.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_fsborite_tgl),
+                            contentDescription = null,
+                            tint = if (favoriteBook) PrimaryLight else GrayBg
+                        )
+                    }
+                    Text(
+                        text = "Agrega una imagen",
+                        modifier = Modifier.align(Alignment.Center),
+                        style = MaterialTheme.typography.caption
                     )
                 }
-                Checkbox(
-                    checked = favoriteBook,
-                    onCheckedChange = {
-                        favoriteBook = it
-                        onCheckBoxSelected(favoriteBook)
-                    },
-                    colors = CheckboxDefaults.colors(
-                        checkedColor = PrimaryColor,
-                        uncheckedColor = Color.Gray,
-                        checkmarkColor = GrayBg,
-                        disabledColor = Color.Gray
-                    )
-                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = dimensionResource(id = R.dimen.padding_6)),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    IconButton(onClick = { launcher.launch("image/*") }) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_gallery_b),
+                            contentDescription = null,
+                            modifier = Modifier.size(50.dp)
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(10.dp))
+                    IconButton(onClick = {}) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_camera),
+                            contentDescription = null,
+                            modifier = Modifier.size(50.dp)
+                        )
+                    }
+                }
                 Spacer(modifier = Modifier.height(14.dp))
                 Row(
                     modifier = Modifier.fillMaxWidth(),
--- app/src/main/java/com/jccsisc/irepcp/utils/ExtensionFunctions.kt
+++ app/src/main/java/com/jccsisc/irepcp/utils/ExtensionFunctions.kt
@@ -9,10 +9,15 @@ import android.view.WindowManager
 import android.view.WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN
 import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
 import android.widget.Toast
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -60,7 +65,7 @@ fun SetNavbarColor(color: Color, useDarkIcons: Boolean = true) {
 /**
  * Leemos una imagen con Coil
  * */
-@Composable
+@Composable //todo crear un data class de tipo generico
 fun setCoilImagePainter(image: String?): AsyncImagePainter = rememberAsyncImagePainter(
     model = ImageRequest.Builder(LocalContext.current)
         .data(image)
