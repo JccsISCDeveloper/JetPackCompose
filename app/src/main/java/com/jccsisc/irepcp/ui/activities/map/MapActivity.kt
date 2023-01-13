0759c77acf8d3ee75e687e12df852dfc8c308883
jccsISC <silva.jccs18@gmail.com>
2023-01-12 23:39:19 -0600

    AGRAGANDO data class para location

--- app/src/main/java/com/jccsisc/irepcp/ui/activities/map/MapActivity.kt
+++ app/src/main/java/com/jccsisc/irepcp/ui/activities/map/MapActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.google.android.gms.location.LocationCallback
 import com.jccsisc.irepcp.R
 import com.jccsisc.irepcp.ui.activities.map.screens.map.MapScreen
 import com.jccsisc.irepcp.ui.theme.GrayBg
@@ -17,6 +18,9 @@ import com.jccsisc.irepcp.utils.GlobalData.transparentNavBar
 import com.jccsisc.irepcp.utils.setColorNavBar
 
 class MapActivity : ComponentActivity() {
+
+    private var locationCallback: LocationCallback? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/jccsisc/irepcp/ui/activities/map/screens/map/model/MyLocation.kt
@@ -0,0 +1,8 @@
+package com.jccsisc.irepcp.ui.activities.map.screens.map.model
+
+/**
+ * Project: IREPCP
+ * FROM: com.jccsisc.irepcp.ui.activities.map.screens.map.model
+ * Created by Julio Cesar Camacho Silva on 10/01/23
+ */
+data class MyLocation(val latitude: Double, val longitude: Double)
